40cb65b67e0c1a176740d8e5d58bd3932218e21b
Vladimir2133 <46980843+Vladimir2133@users.noreply.github.com>
2022-05-31 10:55:11 +0300

    Add files via upload

--- src/components/tooltip/Tooltip.js
+++ src/components/tooltip/Tooltip.js
@@ -5,7 +5,7 @@ import './styleTooltip.css'
 
 export const Tooltip = (props) => {
     return(
-        <ReactTooltip  className ='tooltip' place='bottom' id={props.id}>
+        <ReactTooltip place='bottom' id={props.id}>
             {props.id === props.text &&<span className='tooltip_text'>{props.text}</span>}           
         </ReactTooltip>
         
